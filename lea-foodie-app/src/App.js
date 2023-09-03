59cc05df750a45f4a3f6cd78080e2584af4310c9
THEONE\colon <colond06@gmail.com>
2023-09-03 10:32:17 +0800

    added Header

--- lea-foodie-app/src/App.js
+++ lea-foodie-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home/Home.js'
 function App() {
   return (
     <div>
--- lea-foodie-app/src/index.js
+++ lea-foodie-app/src/index.js
@@ -4,23 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'https://fonts.googleapis.com';
-import 'https://fonts.gstatic.com';
-import 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap';
 import './assets/vendor/bootstrap/css/bootstrap.min.css';
 import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import './assets/vendor/aos/aos.css';
-import './assets/vendor/glightbox/css/glightbox.min.css';
-import './assets/vendor/swiper/swiper-bundle.min.css';
 import './assets/css/main.css';
 
-
 import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
-import './assets/vendor/aos/aos.js';
-import './assets/vendor/glightbox/js/glightbox.min.js';
-import './assets/vendor/purecounter/purecounter_vanilla.js';
-import './assets/vendor/swiper/swiper-bundle.min.js';
-import './assets/vendor/php-email-form/validate.js';
 import './assets/js/main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
