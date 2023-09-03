72b71cc34a4cb2a416fce5b60ceeec9d308e49af
THEONE\colon <colond06@gmail.com>
2023-09-03 13:11:41 +0800

    added about us

--- lea-foodie-app/src/App.js
+++ lea-foodie-app/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home.js'
 import Header from './Components/Header/Header.js';
-
+import About from './Components/About/About.js';
 function App() {
   return (
     <div>
        <Header/>
        <Home/>
-       {/* <About/>
+       <About/>
+       {/* 
        <Menu/>
        <Gallery/>
        <Contact/> */}
