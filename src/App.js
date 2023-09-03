522d922d9536c60a9b9cae6f9db280dba133d734
Lorie Lie Cubid <eirol1221@gmail.com>
2023-09-03 21:06:49 +0800

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Menu from './Components/Menu/Menu.js';
 import About from './Components/About/About.js';
 import Gallery from './Components/Gallery/Gallery.js';
 import Contact from './Components/Contact/Contact.js';
+import Footer from './Components/Footer/Footer.js';
 import AOS from 'aos';
 import '../node_modules/aos/dist/aos.css';
 import React,{useEffect} from "react";
@@ -22,6 +23,7 @@ function App() {
        <Menu/>
        <Gallery/>
        <Contact/>
+       <Footer/>
     </div>
   );
 }
